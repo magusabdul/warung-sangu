5a7e1cf9d9e0c775c4d1d1b64e7701a70f16a15b
Agus Abdul Malik <68399903+magusabdul@users.noreply.github.com>
2021-12-19 21:38:11 +0700

    fix add BundleAnalyzerPlugin
    fix add BundleAnalyzerPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -86,10 +86,10 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(
+    new BundleAnalyzerPlugin({
       analyzerMode: 'disabled',
       generateStatsFile: true,
       statsOptions: { source: false },
-    ),
+    }),
   ],
 };
